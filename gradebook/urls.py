375eea231b6899e5d6277a93a843b76cd43c0c53
Annzxy <kilizxy@gmail.com>
2023-05-07 17:51:56 +1200

    add views for semester

--- gradebook/urls.py
+++ gradebook/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
 from . import views
+from .views import SemesterList, SemesterCreate, SemesterUpdate, SemesterDelete, SemesterDetail
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:id>', views.view_student, name='view_student'),
+    path("semester_list/", SemesterList.as_view(), name="semester_list"),
+    path("semester_create/", SemesterCreate.as_view(), name="semester_create"),
+    path("semester_update/<int:pk>/", SemesterUpdate.as_view(), name="semester_update"),
+    path("semester_delete/<int:pk>/", SemesterDelete.as_view(), name="semester_delete"),
+    path("semester_detail/<int:pk>/", SemesterDetail.as_view(), name="semester_detail"),
 ]
--- gradebook/views.py
+++ gradebook/views.py
@@ -1,10 +1,8 @@
 from django.shortcuts import render
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
 from django.http import HttpResponseRedirect
-from .models import Student
-from .models import StudentEnrollment
-from .models import Lecturer
-from .models import Class
+from .models import Student, StudentEnrollment, Lecturer, Class, Semester
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
 # Create your views here.
 def index(request):
@@ -16,3 +14,27 @@ def view_student(request, id):
     student = Student.objects.get(pk=id)
     return HttpResponseRedirect(reverse('index'))
 
+class SemesterList(ListView):
+    model = Semester
+    template_name = "Semester_List.html"
+
+class SemesterCreate(CreateView):
+    model = Semester
+    template_name = "Semester_Create.html"
+    fields = "__all__"
+    success_url = reverse_lazy("semester_list")
+
+class SemesterUpdate(UpdateView):
+    model = Semester
+    template_name = "Semester_Update.html"
+    fields = "__all__"
+    success_url = reverse_lazy("semester_list")
+
+class SemesterDetail(DetailView):
+    model = Semester
+    template_name = "Semester_Detail.html"
+
+class SemesterDelete(DeleteView):
+    model = Semester
+    template_name = "Semester_Delete.html"
+    success_url = reverse_lazy("semester_list")
