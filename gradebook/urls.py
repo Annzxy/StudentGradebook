68938ab4e18c3a9472941e65992881a16bc00cb8
Annzxy <kilizxy@gmail.com>
2023-05-08 21:01:09 +1200

    add views for semester

--- gradebook/urls.py
+++ gradebook/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
-from .views import SemesterList, SemesterCreate, SemesterUpdate, SemesterDelete, SemesterDetail
+from .views import SemesterList, SemesterCreate, SemesterUpdate, SemesterDelete, SemesterDetail, CourseList, \
+    CourseCreate, CourseUpdate, CourseDelete, CourseDetail, LecturerList, LecturerCreate, LecturerUpdate, \
+    LecturerDelete, LecturerDetail, ClassList, ClassCreate, ClassUpdate, ClassDetail, ClassDelete
 
 urlpatterns = [
     path('', views.index, name='index'),
@@ -10,4 +12,19 @@ urlpatterns = [
     path("semester_update/<int:pk>/", SemesterUpdate.as_view(), name="semester_update"),
     path("semester_delete/<int:pk>/", SemesterDelete.as_view(), name="semester_delete"),
     path("semester_detail/<int:pk>/", SemesterDetail.as_view(), name="semester_detail"),
+    path("course_list/", CourseList.as_view(), name="course_list"),
+    path("course_create/", CourseCreate.as_view(), name="course_create"),
+    path("course_update/<int:pk>/", CourseUpdate.as_view(), name="course_update"),
+    path("course_delete/<int:pk>/", CourseDelete.as_view(), name="course_delete"),
+    path("course_detail/<int:pk>/", CourseDetail.as_view(), name="course_detail"),
+    path("lecturer_list/", LecturerList.as_view(), name="lecturer_list"),
+    path("lecturer_create/", LecturerCreate.as_view(), name="lecturer_create"),
+    path("lecturer_update/<int:pk>/", LecturerUpdate.as_view(), name="lecturer_update"),
+    path("lecturer_delete/<int:pk>/", LecturerDelete.as_view(), name="lecturer_delete"),
+    path("lecturer_detail/<int:pk>/", LecturerDetail.as_view(), name="lecturer_detail"),
+    path("class_list/", ClassList.as_view(), name="class_list"),
+    path("class_create/", ClassCreate.as_view(), name="class_create"),
+    path("class_update/<int:pk>/", ClassUpdate.as_view(), name="class_update"),
+    path("class_delete/<int:pk>/", ClassDelete.as_view(), name="class_delete"),
+    path("class_detail/<int:pk>/", ClassDetail.as_view(), name="class_detail"),
 ]
--- gradebook/views.py
+++ gradebook/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.urls import reverse, reverse_lazy
 from django.http import HttpResponseRedirect
-from .models import Student, StudentEnrollment, Lecturer, Class, Semester
+from .models import Student, StudentEnrollment, Lecturer, Class, Semester, Course
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
 # Create your views here.
@@ -38,3 +38,78 @@ class SemesterDelete(DeleteView):
     model = Semester
     template_name = "Semester_Delete.html"
     success_url = reverse_lazy("semester_list")
+
+class CourseList(ListView):
+    model = Course
+    template_name = "Course_List.html"
+
+class CourseCreate(CreateView):
+    model = Course
+    template_name = "Course_Create.html"
+    fields = "__all__"
+    success_url = reverse_lazy("course_list")
+
+class CourseUpdate(UpdateView):
+    model = Course
+    template_name = "Course_Update.html"
+    fields = "__all__"
+    success_url = reverse_lazy("course_list")
+
+class CourseDetail(DetailView):
+    model = Course
+    template_name = "Course_Detail.html"
+
+class CourseDelete(DeleteView):
+    model = Course
+    template_name = "Course_Delete.html"
+    success_url = reverse_lazy("course_list")
+
+class LecturerList(ListView):
+    model = Lecturer
+    template_name = "Lecturer_List.html"
+
+class LecturerCreate(CreateView):
+    model = Lecturer
+    template_name = "Lecturer_Create.html"
+    fields = "__all__"
+    success_url = reverse_lazy("lecturer_list")
+
+class LecturerUpdate(UpdateView):
+    model = Lecturer
+    template_name = "Lecturer_Update.html"
+    fields = "__all__"
+    success_url = reverse_lazy("lecturer_list")
+
+class LecturerDetail(DetailView):
+    model = Lecturer
+    template_name = "Lecturer_Detail.html"
+
+class LecturerDelete(DeleteView):
+    model = Lecturer
+    template_name = "Lecturer_Delete.html"
+    success_url = reverse_lazy("lecturer_list")
+
+class ClassList(ListView):
+    model = Class
+    template_name = "Class_List.html"
+
+class ClassCreate(CreateView):
+    model = Class
+    template_name = "Class_Create.html"
+    fields = "__all__"
+    success_url = reverse_lazy("class_list")
+
+class ClassUpdate(UpdateView):
+    model = Class
+    template_name = "Class_Update.html"
+    fields = "__all__"
+    success_url = reverse_lazy("class_list")
+
+class ClassDetail(DetailView):
+    model = Class
+    template_name = "Class_Detail.html"
+
+class ClassDelete(DeleteView):
+    model = Class
+    template_name = "Class_Delete.html"
+    success_url = reverse_lazy("class_list")
